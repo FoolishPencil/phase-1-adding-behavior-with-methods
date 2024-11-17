d9fb4851fa84b81f01ca5090e5fd6c8670ccd201
daniel lance <poorfoolspencil@gmail.com>
2024-11-17 13:34:27 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// Your code here
+class Cat {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+
+    speak() {
+        return `${this.name} says meow!`;
+    }
+};
+
+class Dog {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+
+    speak() {
+        return `${this.name} says woof!`;
+    }
+}
+
+class Bird {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+
+    speak() {
+        if (this.sex === "male") {
+            return `It's me! ${this.name}, the parrot!`;
+        } else {
+            return `${this.name} says squawk!`;
+        }
+    }
+}
+
+// Cat Instances
+const cat1 = new Cat("Whiskers", "female");
+console.log(cat1.speak()); // => Whiskers says meow!
+
+// Dog Instances
+const dog1 = new Dog("Fido", "male");
+console.log(dog1.speak()); // => Fido says woof!
+
+// Bird Instances
+const bird1 = new Bird("Polly", "female");
+console.log(bird1.speak()); // => Polly says squawk!
+
+const bird2 = new Bird("Rio", "male");
+console.log(bird2.speak()); // => It's me! Rio, the parrot!
+
